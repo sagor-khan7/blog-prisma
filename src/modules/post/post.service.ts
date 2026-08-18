8024b460a80d3a73c2a17dea42d8faec50c94aa1
sagor khan <sksagor791@gmail.com>
2026-08-18 12:00:18 +0600

    feat: added comment count and ordering

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -140,21 +140,25 @@ const getPostById = async (postId: string) => {
             parentId: null,
             status: CommentStatus.APPROVED,
           },
+          orderBy: { createdAt: "desc" },
           include: {
             replies: {
               where: {
                 status: CommentStatus.APPROVED,
               },
+              orderBy: { createdAt: "asc" },
               include: {
                 replies: {
                   where: {
                     status: CommentStatus.APPROVED,
                   },
+                  orderBy: { createdAt: "asc" },
                 },
               },
             },
           },
         },
+        _count: { select: { comments: true } },
       },
     });
     return postData;
